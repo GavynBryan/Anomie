bc21fb0608ab3ea6fee333ad53429b170f9c2138
Gavyn Bryan <gavynb67@protonmail.com>
2023-07-03 13:33:10 -0400

    Custom Object pools

---
+++ Assets/Scripts/Management/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    private static object lockObject = new object();
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null) {
+                new GameObject();
+            }
+
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance != null && instance != this) {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/Management/ObjectPoolManager.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ObjectPoolManager 
+{
+    private HashSet<ObjectPool> objectPools;
+
+    public GameObject SpawnFromPool(GameObject _obj)
+    {
+        ObjectPool pool = objectPools.Where(x => x.Prefab == _obj).FirstOrDefault();
+        
+}
